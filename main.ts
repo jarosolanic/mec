982916afce67354ecc78dfe5000a6158ccbd9c07
jarosolanic <55108417+jarosolanic@users.noreply.github.com>
2023-02-12 08:48:16 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("DANIEL")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.TShirt)
+})
+basic.forever(function () {
+	
+})
